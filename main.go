566eeb3da42e3c51e62aa0c47f2779d3e0631872
ubulllka <ulyana.bulgakovaa@yandex.ru>
2023-10-30 03:58:31 +0300

    uf2

--- main.go
+++ main.go
@@ -19,5 +19,5 @@ func main() {
     router.Use(cors.New(corsConfig))
     config.DatabaseConnection()
     routes.ContactRouter(router)
-    router.Run(os.Getenv("URL_BACK"))
+    router.Run(":80")
 }
